fc9d10852a52c357dcde3e58652b92ae59e4a579
Kyle <kyle@kyle-main.(none)>
2009-09-18 15:17:32 -0700

    cleaned up musicseek.c

--- musicplayer/src/musicseek.c
+++ musicplayer/src/musicseek.c
@@ -3,15 +3,27 @@
 #include "music-seek.h"
 #include <gtk/gtk.h>
 G_DEFINE_TYPE (MusicSeek, music_seek, GTK_TYPE_HSCALE);
-static void ChangeScroll                     ( gpointer         user_data);
-static gchar * PrintTime (GtkScale *scale,
-		   gdouble   arg1,
-			  gpointer  user_data);
-static gboolean ValueChanged (GtkRange     *range,
-		       GtkScrollType scroll,
-		       gdouble       value,
-			      gpointer      user_data);
+static void 
+change_scroll ( gpointer         user_data);
 
+static gchar * 
+print_time (GtkScale *scale,
+            gdouble   arg1,
+            gpointer  user_data);
+
+static gboolean 
+value_changed (GtkRange     *range,
+               GtkScrollType scroll,
+               gdouble       value,
+               gpointer      user_data);
+static gboolean        
+button_pressed  (GtkWidget      *widget,
+                GdkEventButton *event,
+                gpointer        user_data);
+static gboolean
+button_released(GtkWidget      *widget,
+                GdkEventButton *event,
+                gpointer        user_data);
 
 
 static void
@@ -47,8 +59,11 @@ music_seek_class_init (MusicSeekClass *klass)
 static void
 music_seek_init (MusicSeek *self)
 {
+    MusicSeek *me = self;
+
     gtk_scale_set_value_pos(GTK_SCALE(self),GTK_POS_TOP);
-     
+
+  
 }
 
 
@@ -63,32 +78,66 @@ music_seek_new (void)
 GtkWidget *
 music_seek_new_with_adj_and_player(GtkAdjustment *ad,GsPlayer *player)
 {
-     GtkWidget *curr;
-     MusicSeek *me;
-  curr = g_object_new (MUSIC_TYPE_SEEK, NULL);
-  
-  me= MUSIC_SEEK(curr);
+    GtkWidget *curr;
+    MusicSeek *me;
+    curr = g_object_new (MUSIC_TYPE_SEEK, NULL);
+    me = MUSIC_SEEK(curr);
+    me->adj = ad;
+    me->player = player;
+      gtk_range_set_adjustment(GTK_RANGE(me),me->adj);
+    gtk_range_set_update_policy(GTK_RANGE(me),GTK_UPDATE_DELAYED);
 
-  gtk_range_set_adjustment(GTK_RANGE(curr),ad);
-    	gtk_range_set_update_policy(GTK_RANGE(me),GTK_UPDATE_DELAYED);
-  me->player = player;
+
+    g_signal_connect ((gpointer) GTK_WIDGET(me), "change-value",
+                      (gpointer)value_changed,
+                      (gpointer)(me->player));
+
+    g_signal_connect ((gpointer) GTK_WIDGET(me), "format-value",
+                      (gpointer)print_time,
+                      (gpointer)(me->player));
+
+    //hack to make the left click behave as middle click
+    g_signal_connect ((gpointer) GTK_WIDGET(me), "button-press-event",
+                      (gpointer)button_pressed,
+                      (gpointer)(me->player));
+
+    g_signal_connect ((gpointer) GTK_WIDGET(me), "button-release-event",
+                      (gpointer)button_released,
+                      (gpointer)(me->player));
+    //end hack
+    g_timeout_add (200,(gpointer)change_scroll, me->player);
+     
+
+    return curr;
     
-
-  g_signal_connect ((gpointer) GTK_WIDGET(me), "change-value",
-                  (gpointer)ValueChanged,
-                 (gpointer)(me->player));
-
-  g_signal_connect ((gpointer) GTK_WIDGET(me), "format-value",
-                  (gpointer)PrintTime,
-                 (gpointer)(me->player));
-
-  g_timeout_add (200,(gpointer)ChangeScroll, me->player);
-  
-  return curr;
-  
+}
+static gboolean        
+button_pressed  (GtkWidget      *widget,
+                GdkEventButton *event,
+                gpointer        user_data)
+{
+    GsPlayer *player = (GsPlayer *) user_data;
+    gs_pause(player);
+    event->button = 2;
+    
+    return FALSE;
+    
+}
+static gboolean
+button_released(GtkWidget      *widget,
+                GdkEventButton *event,
+                gpointer        user_data)
+{
+    GsPlayer *player = (GsPlayer *) user_data;
+    gs_pauseResume(player);
+    event->button = 2;
+    return FALSE;
+    
 }
 
-static void ChangeScroll                     ( gpointer         user_data)
+
+static void 
+change_scroll( gpointer  user_data)
 
 {
 
@@ -106,47 +155,45 @@ static void ChangeScroll                     ( gpointer         user_data)
      
 }
 
-static gchar * PrintTime (GtkScale *scale,
-		   gdouble   arg1,
-		   gpointer  user_data)
-     
-{
-     GsPlayer *player = (GsPlayer *) user_data;
-     gchar *str;
+static gchar * 
+print_time (GtkScale *scale,
+            gdouble   arg1,
+            gpointer  user_data)
 
-     str = (gchar *) g_malloc(sizeof(gchar)*50);
-     
-     
-     if(gs_CurrTime(player,str) == TRUE){
-	  
-	       	       return str;
-     }else{
-	  g_strlcpy(str,"0:00 of 0:00",50);
-	  return str;
-	  // return NULL
-	       //  g_free(str);
-	    gs_pauseResume(player);
-     }
+{
+    GsPlayer *player = (GsPlayer *) user_data;
+    gchar *str;
+
+    str = (gchar *) g_malloc(sizeof(gchar)*50);
+
+
+    if(gs_CurrTime(player,str) == TRUE){
+
+        return str;
+    }
+    else
+    {
+        g_strlcpy(str,"0:00 of 0:00",50);
+        return str;
+        gs_pauseResume(player);
+    }
 
 
 }
-static gboolean ValueChanged (GtkRange     *range,
-		       GtkScrollType scroll,
-		       gdouble       value,
-		       gpointer      user_data)
+static gboolean 
+value_changed (GtkRange     *range,
+               GtkScrollType scroll,
+               gdouble       value,
+               gpointer      user_data)
 {
-	GsPlayer *player = (GsPlayer *) user_data;
+    GsPlayer *player = (GsPlayer *) user_data;
 
 
+    if(value > 100 || value <= 0 ||!gs_SeakFromPercent(player,value)) {
+        g_print ("Seek failed!\n");
+        return FALSE;
+    }
 
-   		 //gs_pause(player);
-
-
- if(value > 100 || value <= 0 ||!gs_SeakFromPercent(player,value)) {
-     g_print ("Seek failed!\n");
-     return FALSE;
-     }
-//}
 return FALSE;
 
 }
